4bd4891fca381e18a2af05d8c868f49d3918b536
Tor Soya <torsoya@gmail.com>
2014-12-12 09:41:11 +0100

    change pass works
    change pass works with respons to user so user knows that pass word has
    been changed

--- Glemt_kode.aspx
+++ Glemt_kode.aspx
@@ -1,10 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Glemt_kode.aspx.cs" Inherits="Default2" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .OBS {
+            color:red;
+            font-weight:bold;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <h3>Her kan du få resat dit password!</h3>
-    <asp:Label ID="Resat_label" runat="server" Visible="false" Text="Test1"></asp:Label>
+    <asp:Label CssClass="OBS" ID="Resat_Label1" runat="server" Visible="false" Text=""></asp:Label>
     <br />
     <asp:TextBox CssClass="login_input" required="required" ID="res_Username" style="text-align:center" value="Indtast Brugernavn" onfocus="if (this.value == 'Indtast Brugernavn') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Indtast Brugernavn';}" runat="server" Width="22%"></asp:TextBox>
     <br />
--- Glemt_kode.aspx.cs
+++ Glemt_kode.aspx.cs
@@ -6,10 +6,18 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 
+
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if (res_password.Text == conf_res_password.Text )
+        //{
+        //    Resat_Label1.Text = "test";
+        //    Resat_Label1.Visible = true;
+        //}
+        
+        
 
     }
 
@@ -19,38 +27,50 @@ public partial class Default2 : System.Web.UI.Page
     }
     protected void Ress_password_Click(object sender, EventArgs e)
     {
+
+
+        
+        
         if (res_password.Text == conf_res_password.Text)
         {
+            int affectedrows;
             SqlConnection DBCon = new SqlConnection("Data Source=RDK100938;Initial Catalog=Skole;Integrated Security=True");
-            SqlCommand Sqlupdate = new SqlCommand("UPDATE Users SET Userpwd = '" + res_password.Text + "' where Username = '" + res_Username.Text + "' and Email = '" + res_email.Text + "'", DBCon);
-            Sqlupdate.Connection.Open();
-            Sqlupdate.ExecuteNonQuery();
-            Sqlupdate.Connection.Close();
-            Sqlupdate.Connection.Dispose();
+            SqlCommand Sqlcheckuser = new SqlCommand("Select * From Users Where Username = '" + res_Username.Text + "' and Email = '"+res_email.Text+"'", DBCon);
+            Sqlcheckuser.Connection.Open();
+            affectedrows = Sqlcheckuser.ExecuteNonQuery();
+            Sqlcheckuser.Connection.Close();
+            if (affectedrows == 1)
+            {
+                // updatere password i databasen
+                SqlCommand Sqlupdate = new SqlCommand("UPDATE Users SET Userpwd = '" + res_password.Text + "' where Username = '" + res_Username.Text + "' and Email = '" + res_email.Text + "'", DBCon);
+                Sqlupdate.Connection.Open();
+                Sqlupdate.ExecuteNonQuery();
+                Sqlupdate.Connection.Close();
+                Sqlupdate.Connection.Dispose();
 
-            Resat_label.Visible = true;
-            Resat_label.Text = "Brugeren " + res_Username.Text + " har fået ændret sit password";
-            
-
-
-            res_Username.Text = "";
-            res_password.Text = "";
-            conf_res_password.Text = "";
-            res_email.Text = "";
-
-            System.Threading.Thread.Sleep(3500);
-            Response.Redirect("Default.aspx");
-
-
+                // informere brugeren om det er lykkesed eller ej
+                Resat_Label1.Text = "Brugeren " + res_Username.Text + " har fået ændret kodeord";
+                Resat_Label1.Visible = true;
+                res_Username.Text = "";
+                res_password.Text = "";
+                conf_res_password.Text = "";
+                res_email.Text = "";
+                Response.AddHeader("REFRESH", "2;URL=Default.aspx");
+            }
+            else
+            {
+                Resat_Label1.Visible = true;
+                Resat_Label1.Text = "Brugeren fandtes ikke";
+                res_Username.Text = "";
+                res_email.Text = "";
+                res_password.Text = "";
+                conf_res_password.Text = "";
+            }
         }
-
-
-
-
         else
         {
-            Resat_label.Visible = true;
-            Resat_label.Text = "De 2 passwords matchede ikke";
+            Resat_Label1.Visible = true;
+            Resat_Label1.Text = "De 2 passwords matchede ikke";
             res_password.Text = "";
             conf_res_password.Text = "";
         }
