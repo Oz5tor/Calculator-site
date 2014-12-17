1e0b4dd7a8bf1a832a1bff8816c8a79b19a04526
Tor Soya <torsoya@gmail.com>
2014-12-17 10:54:27 +0100

    Admin - user change
    Dropdown and error corrections - missing code comments

--- User_Admin.aspx
+++ User_Admin.aspx
@@ -1,15 +1,65 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="User_Admin.aspx.cs" Inherits="Default2" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        td {
+            color:red;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <h4>Dette er Admin</h4>
     <asp:Label ID="Admin_Label" runat="server" Text=""></asp:Label>
     <br />
-    <asp:DropDownList CssClass="login_input" ID="Dropdown_dropdelete" style="text-align:center" runat="server" Width="22%">
+
+
+    
+
+    <asp:DropDownList CssClass="login_input" ID="Dropdown_dropdelete" AutoPostBack="false" style="text-align:center" runat="server" Width="22%">
     </asp:DropDownList>
-    <br />
-        <asp:Button CssClass="button" ID="delete_user" runat="server" Text="Slet bruger" OnClick="delete_user_Click" />
-        <asp:Button CssClass="button" ID="logoff_admin" runat="server" Text="Log af" OnClick="logoff_admin_Click" />
+       <asp:Button CssClass="button" ID="select_user" runat="server" Text="Vælg Bruger" OnClick="select_user_Click" />
+<center>
+<table>
+        <tr>
+            <td><asp:Label Visible="false" ID="user_id_lable" runat="server" Text="Bruger ID"></asp:Label></td>
+            <td><asp:TextBox Visible="false" ReadOnly="true" Cssclass="login_input" ID="user_id" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label Visible="false" ID="Brugernavn" runat="server" Text="Brugernavn"></asp:Label></td>
+            <td><asp:TextBox Visible="false" Cssclass="login_input" ID="Edit_username" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label Visible="false" ID="Kodeord" runat="server" Text="Kodeord"></asp:Label></td>
+            <td><asp:TextBox Visible="false" Cssclass="login_input" ID="Edit_pwd" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label Visible="false" ID="Addresse" runat="server" Text="Addresse"></asp:Label></td>
+            <td><asp:TextBox Visible="false" Cssclass="login_input" ID="Edit_adress" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label Visible="false" ID="Email" runat="server" Text="Email"></asp:Label></td>
+            <td><asp:TextBox Visible="false" Cssclass="login_input" ID="Edit_email" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label Visible="false" ID="Brugertype" runat="server" Text="Brugertype"></asp:Label>  </td>
+            <td>
+                <asp:DropDownList CssClass="login_input" Visible="false" ID="Edit_usertype" style="text-align:center" runat="server" Width="70%">
+                    <asp:ListItem Text="Admin" Value="Admin" />
+                    <asp:ListItem Text="Sub" Value="Sub" />
+                    <asp:ListItem Text="Calc" Value="Calc" />
+                    <asp:ListItem Text="SubCalc" Value="SubCalc" />
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Button Visible="false" CssClass="button" ID="update_user" runat="server" Text="Opdater bruger" OnClick="update_user_Click" />
+                <asp:Button Visible="false" CssClass="button" ID="delete_user" runat="server" Text="Slet bruger" OnClick="delete_user_Click" />
+                <asp:Button CssClass="button" ID="logoff_admin" runat="server" Text="Log af" OnClick="logoff_admin_Click" />
+                <asp:Button CssClass="button" ID="Tilbage_select_user" Visible="false" runat="server" Text="Tilbage" OnClick="Tilbage_select_user_Click" />
+            </td>
+        </tr>
+    </table>
+</center>
 </asp:Content>
 
--- User_Admin.aspx.cs
+++ User_Admin.aspx.cs
@@ -18,11 +18,15 @@ public partial class Default2 : System.Web.UI.Page
             SqlCommand SQLCmd = new SqlCommand("select Username from Users", DBCon);
             SQLCmd.Connection.Open();
             SqlDataReader Reader = SQLCmd.ExecuteReader();
+            
 
             while (Reader.Read())
             {
                 Dropdown_dropdelete.Items.Add(Reader["Username"].ToString());
+                
             }
+            SQLCmd.Connection.Close();
+            SQLCmd.Connection.Dispose();
         }
         else
         {
@@ -48,5 +52,92 @@ public partial class Default2 : System.Web.UI.Page
         SQLCmd.Connection.Dispose();
         Response.Redirect("User_Admin.aspx");
 
+    }
+    protected void update_user_Click(object sender, EventArgs e)
+    {
+        
+        
+        SqlConnection DBCon1 = new SqlConnection("Data Source=RDK100938;Initial Catalog=Skole;Integrated Security=True");
+        SqlCommand SQLCmd = new SqlCommand("UPDATE Users SET Username = '" + Edit_username.Text.ToString() + "',  Userpwd = '" + Edit_pwd.Text.ToString() + "', Adresse = '" + Edit_adress.Text.ToString() + "', Email = '" + Edit_email.Text.ToString() + "', Usertype = '" + Edit_usertype.Text.ToString() + "' where UserID = '" + user_id.Text + "'", DBCon1);
+        SQLCmd.Connection.Open();
+        SQLCmd.ExecuteNonQuery();
+        SQLCmd.Connection.Close();
+        SQLCmd.Connection.Dispose();
+        
+
+    }
+    protected void select_user_Click(object sender, EventArgs e)
+    {
+        SqlConnection DBCon2 = new SqlConnection("Data Source=RDK100938;Initial Catalog=Skole;Integrated Security=True");
+        SqlCommand SQLCmd = new SqlCommand("select * from Users where Username = '" + Dropdown_dropdelete.Text+ "'", DBCon2);
+        SQLCmd.Connection.Open();
+        SqlDataReader Reader = SQLCmd.ExecuteReader();
+        
+        // labels to visivle
+        Brugernavn.Visible  = true;
+        Email.Visible       = true;
+        Kodeord.Visible     = true;
+        Addresse.Visible    = true;
+        Brugertype.Visible  = true;
+        user_id_lable.Visible = true;
+        // select user dropdown hidden
+        Dropdown_dropdelete.Visible = false;
+        select_user.Visible = false;
+        Tilbage_select_user.Visible = true;
+        // input feilds visible
+        Edit_username.Visible  = true;
+        Edit_email.Visible     = true;
+        Edit_pwd.Visible       = true;
+        Edit_adress.Visible    = true;
+        Edit_usertype.Visible  = true;
+        user_id.Visible = true;
+
+        // admin user functions buttons
+        update_user.Visible = true;
+        delete_user.Visible = true;
+        // set values from database into input feilds
+        while (Reader.Read())
+        {
+
+            user_id.Text = Reader["UserID"].ToString();
+            Edit_username.Text = Reader["Username"].ToString();
+            Edit_email.Text = Reader["Email"].ToString();
+            Edit_pwd.Text = Reader["Userpwd"].ToString();
+            Edit_adress.Text = Reader["Adresse"].ToString();
+            Edit_usertype.Text = Reader["Usertype"].ToString();
+            
+        }
+    }
+    protected void Tilbage_select_user_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("User_Admin.aspx");
+        // labels to visivle
+        Brugernavn.Visible = false;
+        Email.Visible = false;
+        Kodeord.Visible = false;
+        Addresse.Visible = false;
+        Brugertype.Visible = false;
+        user_id_lable.Visible = false;
+        // select user dropdown hidden
+        Dropdown_dropdelete.Visible = true;
+        select_user.Visible = true;
+        // input feilds visible
+        Edit_username.Visible = false;
+        Edit_email.Visible = false;
+        Edit_pwd.Visible = false;
+        Edit_adress.Visible = false;
+        Edit_usertype.Visible = false;
+        user_id.Visible = false;
+
+        // admin user functions buttons
+        update_user.Visible = false;
+        delete_user.Visible = false;
+        Tilbage_select_user.Visible = false;
+        // clear input feilds
+            Edit_username.Text = "";
+            Edit_email.Text = "";
+            Edit_pwd.Text = "";
+            Edit_adress.Text = "";
+            Edit_usertype.Text = "";
     }
 }
